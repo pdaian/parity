1321887062305a8851ff5cafc5074dab376ecece
Philip Daian <phil@linux.com>
2016-12-09 19:57:22 +0000

    add cli to listen

--- listener/run.py
+++ listener/run.py
@@ -1,16 +1,16 @@
-from flask import request, jsonify, Flask
+from flask import request, jsonify, Flask, sys
 app = Flask(__name__)
 
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
     ip = request.remote_addr
-    nodes = open('/home/ubuntu/nodes', 'r').read()
+    nodes = open(sys.argv[1] + 'nodes', 'r').read()
     # Give em a fresh address
-    id = int(open('/home/ubuntu/counter').read().strip())
-    open('/home/ubuntu/counter', 'w').write(str(id + 1))
+    id = int(open(sys.argv[1] + 'counter').read().strip())
+    open(sys.argv[1] + 'counter', 'w').write(str(id + 1))
     # Track IP
     if not ip in nodes:
-        open('/home/ubuntu/nodes', 'a').write(ip + '\n')
+        open(sys.argv[1] + 'nodes', 'a').write(ip + '\n')
     return str(id % 100)
 
 if __name__ == '__main__':
